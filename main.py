7afad3d0f6d73754a390d3d8d2c0cbccd10c2fe3
austinmleo <austinmleo@gmail.com>
2017-12-08 14:03:21 -0700

    Gets filenames from directory.

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 import sys
 import os
 
+def getFilenames(path):
+    files = []
+
+    for r, d, f in os.walk(path):
+        for name in f:
+            files.append(os.path.join(r, name))
+
+    return files
+
 def main():
     
-    path = sys.args[1]
-    
+    files = getFilenames('./20_newsgroups/')
+             
+    print files
+    print len(files)
     exit()
 
 
-if __name__ == 'main':
+if __name__ == '__main__':
     main()
 
 
