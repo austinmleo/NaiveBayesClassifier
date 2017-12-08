2d75fb2207dcb41d5f4794ddac33328fed5987d1
austinmleo <austinmleo@gmail.com>
2017-12-08 15:12:53 -0700

    Gets word count of files.

--- main.py
+++ main.py
@@ -2,20 +2,46 @@ import sys
 import os
 
 def getFilenames(path):
-    files = []
-
+    files = {}
+    
     for r, d, f in os.walk(path):
         for name in f:
-            files.append(os.path.join(r, name))
+            files[os.path.join(r,name)] = r
 
     return files
+
+def stripchars(line):
+    # Characters to strip
+    chars = ['.', ' ', '\n', '--', '!']
+
+    for c in chars:
+        line = line.replace(c, ' ')
+
+    return line
+
+
+def getWordcount(filename):
+    filereader = open(filename, 'r')
+
+    words = {}
+
+    for line in filereader:
+        line = stripchars(line).split(' ')
+        for word in line:
+            word = word.lower()
+            if word != '':
+                if word in words:
+                    words[word] += 1
+                else:
+                    words[word] = 1
+
 
 def main():
     
     files = getFilenames('./20_newsgroups/')
-             
-    print files
+    
     print len(files)
+    getWordcount(files.popitem()[0])
     exit()
 
 
